ff93aa69dcd6c28cde09e370ca3d8a8cb331f285
KaelShowTile <kaelshowtile@gmail.com>
2025-09-10 10:02:13 +1000

    2025091002

--- functions.php
+++ functions.php
@@ -950,10 +950,12 @@ function cht_update_cart_fragments($fragments) {
 }
 
 //change the title of stripe payment
-add_filter('woocommerce_gateway_title', 'change_stripe_gateway_title', 10, 2);
-function change_stripe_gateway_title($title, $id) {
-    if ($id === 'stripe') {
-        return __('Credit Cards', 'your-text-domain');
-    }
-    return $title;
+
+
+function change_stripe_gateway_title( $translation, $text, $domain ) {
+	if ( $domain == 'woocommerce-payments' && $text == 'stripe' ) {
+		$translation = 'Credit Cards';
+	}
+	return $translation;
 }
+add_filter( 'gettext_woocommerce-payments', 'change_stripe_gateway_title', 999, 3 );
