6323dedd821be9519cc45c5bbbff1ee0a86daadf
KaelShowTile <kaelshowtile@gmail.com>
2025-09-12 15:21:27 +1000

    2025091216

--- single-tile-collection.php
+++ single-tile-collection.php
@@ -70,7 +70,7 @@
 						echo '</h3>';
 					}else{
 						
-						echo'<h3>' . get_the_title( $collection_id ) . 'Products</h3>';
+						echo'<h3>' . get_the_title( $collection_id ) . ' Products</h3>';
 					}	
 					
 						// Get query values
